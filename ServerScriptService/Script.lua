c83bd75d3746d833766237e1c6077426bf1704a9
Richard Scott <bububooo3@gmail.com>
2025-11-05 01:49:39 -0500

    Updated ServerScriptService/Script.lua

--- ServerScriptService/Script.lua
+++ ServerScriptService/Script.lua
@@ -3,12 +3,15 @@ local p_indicator = script.point
 
 function makeCyclePoints(area: Part): {number}
 	local cyclePoints = {}
+	local parts = {}
 	
 	-- Get vertices
 	local part_CFrame = area.CFrame
 	local sx, sy, sz = area.Size.X, area.Size.Y, area.Size.Z
 	local max, min = 0, 0
 	local factor = 32
+	local increment = 4
+	local surface_level = script:GetAttribute('surface_level')
 	
 	-- (From NavMesh attempt)
 	--part_CFrame*CFrame.new(part_size.X/2, part_size.Y/2,part_size.Z/2) ----> Front Top Right
@@ -21,36 +24,50 @@ function makeCyclePoints(area: Part): {number}
 	--part_CFrame*CFrame.new(-part_size.X/2, -part_size.Y/2,-part_size.Z/2) ----> Back Top Left
 	--part_CFrame*CFrame.new(part_size.X/2, -part_size.Y/2,part_size.Z/2) ----> Back Bottom Left
 	
-	for x=0, sx do
-		for y=0, sy do
-			for z=0, sz do
+	for x=0, sx, increment do
+		for y=0, sy, increment do
+			for z=0, sz, increment do
 				local index = x + (y-1) * sx + (z-1) * sx * sy
 				local value = math.clamp(math.noise(x/sx,y/sy,z/sz), -1, 1) * factor
+				--if value < surface_level then continue end
 				cyclePoints[index] = value
 				max, min = math.max(max,value), math.min(min,value)
 			end
 		end
 	end
 	
-	for x=0, sx do
-		for y=0, sy do
-			for z=0, sz do
-				local value = math.map(math.noise(x/sx,y/sy,z/sz), -1, 1, 0, 1) * factor
-				
+	for x=0, sx, increment do
+		for y=0, sy, increment do
+			for z=0, sz, increment do
+				local value = math.clamp(math.noise(x/sx,y/sy,z/sz), -1, 1)
+				if (value * factor) < surface_level then continue end
 				local temp = p_indicator:Clone()
-				local n = value/factor
+				local n = math.map(value, -1, 1, 0, 1)
 				temp.Position = Vector3.new(x,y,z) + area.Position - area.Size/2
 				temp.Color = Color3.new(n, n, n)
 				temp.Parent = workspace
-				
+				table.insert(parts, temp)
 			end
 		end
 	end
 	
-	return cyclePoints, NumberRange.new(min, max)
+	local nr = NumberRange.new(min, max)
+	script:SetAttribute("range", NumberRange.new(min, max))
+	return cyclePoints, nr, parts
 end
 
 local surfaceLevel = 0
 local target_space = workspace.Part
-local points, range = makeCyclePoints(target_space)
+local points, range, parts = makeCyclePoints(target_space)
 print(points, range)
+
+local t = 0
+
+game:GetService("RunService").Heartbeat:Connect(function(dt)
+	t += dt
+	
+	if t < .5 then return end
+	t=0
+	for _, v in parts do v:Destroy() end
+	points, range, parts = makeCyclePoints(target_space)
+end)
