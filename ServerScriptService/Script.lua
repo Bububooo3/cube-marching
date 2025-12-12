166df284dd44328c6c052b908a09672f8aec78ea
Richard Scott <bububooo3@gmail.com>
2025-12-12 10:21:52 -0500

    Update Script.lua
    still doesnt work

--- ServerScriptService/Script.lua
+++ ServerScriptService/Script.lua
@@ -1,136 +1,127 @@
--- @ScriptType: Script
--- Here we see Roblox's sandboxing in full swing (ty Roblox)
+--!optimize 2
+
+
 
 -------------------
 -- || GLOBALS || --
 -------------------
-local cubeIndex = 0
-local lookup, tri_table = {}, require(script:WaitForChild("triTable"))
+local tConstants = require(script:WaitForChild("tConstants"))
+local cPoints, ePoints, vPoints = tConstants.cornerTable, tConstants.edgeTable, tConstants.vertexTable
 local AssetService = game:GetService("AssetService")
 local max, min = 0,0
+local vPositions, tIndices, vCache = {}, {}, {} -- right here in your own backyard!
+
 
 -------------------
 --|| FUNCTIONS ||--
 -------------------
+-- Lerp the edge, brotato
+function lerpEdge(p1: Vector3, p2: Vector3, v1: number, v2: number, surface_level: number): Vector3
+	if (v1==v2) then return p1 end ----> They're the same value so dv is 0 & we can't divide by that so return midpoint aka (p1+p2)/2
+	
+	local Dv = v2 - v1 ----> Difference in values (pretend its an uppercase delta)
+	local t = math.clamp((surface_level - v1)/Dv, 0, 1)
+	--^ we include surface_level as a parm instead of getting attribute so if it changes mid fxn we're consistent
+	
+	return p1:Lerp(p2, t)
+	--^ aka p1 + (p2 - p1) * t aka getting point on edge btwn p1 and p2 to return as middle for a vertex point but its not 50/50
+end
+
+
+-- Make some triangles, mikebrosowski
 function makeCyclePoints(area: Part): {number}
-	-- Get vertices
-	local part_CFrame = area.CFrame
-	local sx, sy, sz = area.Size.X, area.Size.Y, area.Size.Z
+	-- Necessities
+	local cubeIndex = 0 ----> Literally forgot this somehow 😢
+	local tVal, tPos = {}, {}
+
+	-- Changeables
 	local factor = 3200
-	local increment = sx --> Just get cube vertices
-	local surface_level = script:GetAttribute('surface_level') or 1
-	local vertex = 0
-	
-	for x=0, sx, increment do
-		for y=0, sy, increment do
-			for z=0, sz, increment do
-				local v = math.clamp(math.noise(area.Position.X*0.132,area.Position.Y*0.132,area.Position.Z*0.134 + os.clock()-math.floor(os.clock())), -1, 1) * factor
-				print(v)
-				lookup[vertex+20] =  v --> Starts @ k=0
-				lookup[vertex+30] = Vector3.new(x,y,z)
-				max, min = math.max(max, v), math.min(min, v)
-				vertex += 1
-			end
-		end
+	local surface_level = script:GetAttribute('surface') or 1
+
+	-- Get vertices & values the smart way ✌
+	for i = 0, 7 do
+		local offset = cPoints[i]
+		local pWorld = area.Position + offset ----> Straight to the corner from pivot (which should be center of cube right)
+		local v = math.clamp(math.noise(pWorld.X * 0.132, pWorld.Y * 0.132, pWorld.Z * 0.134 + os.clock() - math.floor(os.clock())), -1, 1) * factor
+
+		tPos[i], tVal[i] = pWorld, v
+
+		max, min = math.max(max, v), math.min(min, v)
 	end
 
-	-- Convert to standard order
-	lookup[0] = lookup[21]
-	lookup[1] = lookup[24]
-	lookup[2] = lookup[25]
-	lookup[3] = lookup[20]
-	lookup[4] = lookup[23]
-	lookup[5] = lookup[27]
-	lookup[6] = lookup[26]
-	lookup[7] = lookup[22]
-	
-	lookup[10] = lookup[31]
-	lookup[12] = lookup[34]
-	lookup[11] = lookup[35]
-	lookup[13] = lookup[30]
-	lookup[14] = lookup[33]
-	lookup[15] = lookup[37]
-	lookup[16] = lookup[36]
-	lookup[17] = lookup[32]
-
-	-- Triangulation Table (Get triangle vertices)
-	for i=0, 7, 1 do
-		cubeIndex = (lookup[i] >= surface_level) and bit32.bor(cubeIndex, 2^i) or 2
+	-- Triangulation Table
+	-- (Get triangle vertices using bitmask that shows which positions have valid values and can be used as triangle key in base 10 🤓)
+	for i = 0, 7 do
+		cubeIndex = (tVal[i] >= surface_level) and bit32.bor(cubeIndex, bit32.lshift(1, i)) or cubeIndex
 	end
 
-	local triangleEdges = tri_table[cubeIndex]
-	local tPoints: {Vector3} = {}
-	local tInd = 0
-	for _, v: number in triangleEdges do
-		tInd += 1
+	--[[ 
+	It's right this time btw
+		0  btwn 0 and 1
+		1  btwn 1 and 2
+		2  btwn 2 and 3
+		3  btwn 3 and 0
+		4  btwn 4 and 5
+		5  btwn 5 and 6
+		6  btwn 6 and 7
+		7  btwn 7 and 4
+		8  btwn 0 and 4
+		9  btwn 1 and 5
+		10 btwn 2 and 6
+		11 btwn 3 and 7
+	]]
+	
+	-- Get all intersection points all at once for organization purposes
+	local eVertices = {};
+	for i = 0, 11 do
+		local c1, c2 = ePoints[i][1], ePoints[i][2]
+		eVertices[i] = lerpEdge(tPos[c1], tPos[c2], tVal[c1], tVal[c2], surface_level)
+	end
+	
+	local tI = 0  ---> What vertex r we on?
+	local tPoints = {} ---> Triangle data stored temporarily
+	
+	for _, eVal in vPoints[cubeIndex] do
+		if eVal == -1 then break end --> Termination character thing
 		
-		--[[
-			0 btwn 2, 6
-			1 btwn 6, 5
-			2 btwn 1, 5
-			3 btwn 1, 2
-			4 btwn 4, 8
-			5 btwn 8, 7
-			6 btwn 3, 7
-			7 btwn 3, 4
-			8 btwn 2, 4
-			9 btwn 6, 8
-			10 btwn 5, 7
-			11 btwn 1, 3
-		]]
-		-- Make triangles w/ midpoint
-		if v==0 then
-			tPoints[tInd] = (lookup[10]+lookup[15])/2
-		elseif v==1 then
-			tPoints[tInd] = (lookup[14]+lookup[15])/2
-		elseif v==2 then
-			tPoints[tInd] = (lookup[10]+lookup[14])/2
-		elseif v==3 then
-			tPoints[tInd] = (lookup[11]+lookup[10])/2
-		elseif v==4 then
-			tPoints[tInd] = (lookup[13]+lookup[17])/2
-		elseif v==5 then
-			tPoints[tInd] = (lookup[16]+lookup[17])/2
-		elseif v==6 then
-			tPoints[tInd] = (lookup[12]+lookup[16])/2
-		elseif v==7 then
-			tPoints[tInd] = (lookup[12]+lookup[13])/2
-		elseif v==8 then
-			tPoints[tInd] = (lookup[11]+lookup[13])/2
-		elseif v==9 then
-			tPoints[tInd] = (lookup[17]+lookup[15])/2
-		elseif v==10 then
-			tPoints[tInd] = (lookup[14]+lookup[16])/2
-		elseif v==11 then
-			tPoints[tInd] = (lookup[10]+lookup[12])/2
-		end
-		if tInd == 3 then
-			-- Draw triangle from 3 vertex positions
-			local mPart = script.MeshPart:Clone()
-			mPart.Anchored = true
-			mPart.CanCollide = false
-			mPart.Size = area.Size
-			mPart.Position = area.Position
+		table.insert(tPoints, eVertices[eVal])
+		tPoints[tI] = eVertices[eVal]
+		tI += 1
+		
+		if tI == 2 then
+			tI = 0
+			-- much faster mesh generation (single instead of 1 per triangle)			
+			local i1 = #vPositions
+			local i2, i3 = i1+1, i1+2
 			
-			local tMesh = AssetService:CreateEditableMesh()
-			local v1 = tMesh:AddVertex(tPoints[1])
-			local v2 = tMesh:AddVertex(tPoints[2])
-			local v3 = tMesh:AddVertex(tPoints[3])
+			vPositions[i1] = eVertices[tPoints[0]]
+			vPositions[i2] = eVertices[tPoints[1]]
+			vPositions[i3] = eVertices[tPoints[2]]
 			
-			tMesh:AddTriangle(v1,v2,v3)
-			local obj = AssetService:CreateMeshPartAsync(Content.fromObject(tMesh))
-			
-			mPart.Parent = workspace.Triangles
-			mPart:ApplyMesh(obj)
-			
+			table.insert(tIndices, i1)
+			table.insert(tIndices, i2)
+			table.insert(tIndices, i3)			
 			table.clear(tPoints)
-			tInd = 0
 		end
 	end
+	
+	local data = AssetService:CreateEditableMesh()
+	
+	for i, v in (vPositions) do data:AddVertex(v) end
+	
+	for i=0, #tIndices-1, 3 do
+		data:AddTriangle(tIndices[i], tIndices[i+1], tIndices[i+2])
+	end
+	
+	local mPart = script.MeshPart:Clone()
+	local new = AssetService:CreateMeshPartAsync(Content.fromObject(data))
+	mPart:ApplyMesh(new)
+	mPart.Parent = workspace.Triangles
 	
 	script:SetAttribute("range", NumberRange.new(min, max))
 	return true
 end
+
 
 -------------------
 -- || RUNTIME || --
